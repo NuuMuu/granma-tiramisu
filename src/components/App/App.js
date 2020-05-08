61be9bb33894df473614f9a4eaa34f34a9fa3d3e
NuuMuu <shikarew_@naver.com>
2020-05-08 23:36:17 +0900

    commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,34 @@
-import React, { Component } from 'react';
-import 'components/App/App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Nav from "../Nav/Nav";
+import Cart from "../Cart/Cart";
+import Main from "../Main/Main";
+//import Item from "../Item/Item";
+import data from "../../MOCK_DATA.json";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data
+    };
+  };
+  
+  componentDidMount() {
+    console.log(this.state.products)
+  };
+
   render() {
     return (
-      <div className="App">
-        최상단
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/cart" component={Cart} />
+        </Switch>
       </div>
     );
+  };
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "components/App/App";
+import App from "./components/App/App";
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <BrowserRouter>
