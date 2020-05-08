0a4ef558661308951ef9625989a16ca1f6f2821d
NuuMuu <shikarew_@naver.com>
2020-05-08 23:17:50 +0900

    commited

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import 'components/App/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        최상단
+      </div>
+    );
+
+export default App;
