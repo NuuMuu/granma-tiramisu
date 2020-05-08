a8c130d2cf4bace8b236733480e37641cc572187
NuuMuu <shikarew_@naver.com>
2020-05-08 23:54:00 +0900

    Updates

---
+++ precache-manifest.b6156acc5907be98fceb3fb8d6c11319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57210db3ce85f76a4b806766a710e17",
+    "url": "/granma-tiramisu/index.html"
+  },
+  {
+    "revision": "30d0bc02395e7a20c571",
+    "url": "/granma-tiramisu/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a45800f1533da1ea72e6",
+    "url": "/granma-tiramisu/static/css/main.83511bf8.chunk.css"
+  },
+  {
+    "revision": "30d0bc02395e7a20c571",
+    "url": "/granma-tiramisu/static/js/2.046834be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/granma-tiramisu/static/js/2.046834be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45800f1533da1ea72e6",
+    "url": "/granma-tiramisu/static/js/main.90b2be93.chunk.js"
+  },
+  {
+    "revision": "5e1a53d3e159dea3f4b5",
+    "url": "/granma-tiramisu/static/js/runtime-main.21854e73.js"
+  }
+]);
